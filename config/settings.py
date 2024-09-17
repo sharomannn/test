75e88a00599d906793f9bf11ee75e2f1fcb622ba
roman <r.sharipov@itcodegroup.ru>
2024-09-17 08:31:54 +0500

    init

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# config/settings.py
+
+ALLOWED_HOSTS = ['212.193.30.159', 'localhost', '127.0.0.1']
